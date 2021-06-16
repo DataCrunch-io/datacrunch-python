51733d0eb11bd73d4b64c4e96e09ebcb7dc35572
Tamir <tamir@datacrunch.io>
2021-06-16 17:20:31 +0300

    added example for instance actions with volumes

--- examples/instances_and_volumes.py
+++ examples/instances_and_volumes.py
@@ -8,15 +8,46 @@ CLIENT_ID = 'Ibk5bdxV64lKAWOqYnvSi'  # Replace with your client ID
 # Create datcrunch client
 datacrunch = DataCrunchClient(CLIENT_ID, CLIENT_SECRET)
 
-# Create instance with extra volumes
-# TODO:
+# Get some volume type constants
+NVMe = datacrunch.constants.volume_types.NVMe
+HDD = datacrunch.constants.volume_types.HDD
+
+EXISTING_OS_VOLUME_ID = '81e45bf0-5da2-412b-97d7-c20a7564fca0'
+EXAMPLE_VOLUME_ID = '225dde24-ae44-4787-9224-2b9f56f44394'
+EXAMPLE_INSTANCE_ID = '1eeabba4-caf7-4b4a-9143-0107034cc7f5'
+
+# Get all SSH keys
+ssh_keys = datacrunch.ssh_keys.get()
+
+# Create instance with extra attached volumes
+instance_with_extra_volumes = datacrunch.instances.create(instance_type='1V100.6V',
+                                                          image='fastai',
+                                                          ssh_key_ids=ssh_keys[0].id,
+                                                          hostname='example',
+                                                          description='example instance',
+                                                          volumes=[
+                                                                    {"type": HDD, "name": "volume-1", "size": 95},
+                                                                    {"type": NVMe, "name": "volume-2", "size": 95}
+                                                          ])
 
 # Create instance with existing OS volume as an image
-# TODO:
+instance_with_existing_os_volume = datacrunch.instances.create(instance_type='1V100.6V',
+                                                               image=EXISTING_OS_VOLUME_ID,
+                                                               ssh_key_ids=ssh_keys[0].id,
+                                                               hostname='example',
+                                                               description='example instance')
 
-# Delete instance without deleting the OS volume
-# TODO:
+# Delete instance AND OS volume (the rest of the volumes would be detached)
+datacrunch.instances.action(instance_id=EXAMPLE_INSTANCE_ID,
+                            action=datacrunch.constants.instance_actions.DELETE)
 
-# Delete instance and one of it's volumes
-# TODO:
+# Delete instance WITHOUT deleting the OS volume (will detach all volumes of the instance)
+datacrunch.instances.action(instance_id=EXAMPLE_INSTANCE_ID,
+                            action=datacrunch.constants.instance_actions.DELETE,
+                            volume_ids=[])
 
+
+# Delete instance and one of it's volumes (will delete one volume, detach the rest)
+datacrunch.instances.action(instance_id=EXAMPLE_INSTANCE_ID,
+                            action=datacrunch.constants.instance_actions.DELETE,
+                            volume_ids=[EXAMPLE_VOLUME_ID])
