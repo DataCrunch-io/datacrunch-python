41e3cd008d6cec1d4c2a0db0fe84772674829269
Tamir <tamir@datacrunch.io>
2025-05-12 11:44:45 +0300

    small fix to example

--- examples/containers/fileset_secret_example.py
+++ examples/containers/fileset_secret_example.py
@@ -2,7 +2,7 @@ import os
 from datacrunch import DataCrunchClient
 
 # Fileset secrets are a way to mount sensitive files like API keys, certs, and credentials securely inside a container, without hardcoding them in the image or env vars.
-
+# This example demonstrates how to create a fileset secret containing two files from your local filesystem
 
 # Get client secret and id from environment variables
 DATACRUNCH_CLIENT_ID = os.environ.get('DATACRUNCH_CLIENT_ID')
@@ -11,8 +11,8 @@ DATACRUNCH_CLIENT_SECRET = os.environ.get('DATACRUNCH_CLIENT_SECRET')
 # Initialize the client with your credentials
 datacrunch = DataCrunchClient(DATACRUNCH_CLIENT_ID, DATACRUNCH_CLIENT_SECRET)
 
-# Define the secret name and the file paths
-SECRET_NAME = "my_fileset_secret"
+# Define the secret name and the file paths from your local filesystem where this script is running
+SECRET_NAME = "my-fileset-secret"
 RELATIVE_FILE_PATH = "./relative-path/file1.txt"
 ABSOLUTE_FILE_PATH = "/home/username/absolute-path/file2.json"
 
@@ -21,9 +21,8 @@ fileset_secret = datacrunch.containers.create_fileset_secret_from_file_paths(
     secret_name=SECRET_NAME, file_paths=[RELATIVE_FILE_PATH, ABSOLUTE_FILE_PATH])
 
 # Get the secret
-secret = datacrunch.containers.get_fileset_secret(
-    secret_name=SECRET_NAME)
-print(secret)
+secrets = datacrunch.containers.get_fileset_secrets()
+print(secrets)
 
 # Delete the secret
 datacrunch.containers.delete_fileset_secret(secret_name=SECRET_NAME)
