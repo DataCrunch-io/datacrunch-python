1001d17a517b235d0375c1be20fe31764db05e06
Alexey Shamrin <alexey@datacrunch.io>
2025-10-29 15:54:56 +0200

    lint: pytest

--- tests/unit_tests/test_exceptions.py
+++ tests/unit_tests/test_exceptions.py
@@ -7,28 +7,22 @@ ERROR_MESSAGE = 'test message'
 
 
 def test_api_exception_with_code():
-    # arrange
     error_str = f'error code: {ERROR_CODE}\nmessage: {ERROR_MESSAGE}'
 
-    # act
-    with pytest.raises(APIException) as excinfo:
+    with pytest.raises(APIException) as exc_info:
         raise APIException(ERROR_CODE, ERROR_MESSAGE)
 
-        # assert
-        assert excinfo.value.code == ERROR_CODE
-        assert excinfo.value.message == ERROR_MESSAGE
-        assert excinfo.value.__str__() == error_str
+    assert exc_info.value.code == ERROR_CODE
+    assert exc_info.value.message == ERROR_MESSAGE
+    assert exc_info.value.__str__() == error_str
 
 
 def test_api_exception_without_code():
-    # arrange
     error_str = f'message: {ERROR_MESSAGE}'
 
-    # act
-    with pytest.raises(APIException) as excinfo:
+    with pytest.raises(APIException) as exc_info:
         raise APIException(None, ERROR_MESSAGE)
 
-        # assert
-        assert excinfo.value.code is None
-        assert excinfo.value.message == ERROR_MESSAGE
-        assert excinfo.value.__str__() == error_str
+    assert exc_info.value.code is None
+    assert exc_info.value.message == ERROR_MESSAGE
+    assert exc_info.value.__str__() == error_str
